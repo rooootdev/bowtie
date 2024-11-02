d217bf3e5ee18c02d87e58f147f262476ae638f2
roooot <103732419+rooootdev@users.noreply.github.com>
2024-11-02 22:06:58 +0100

    Update bowtie.ps1

--- bowtie.ps1
+++ bowtie.ps1
@@ -1,1 +1,11 @@
+$bowtie = { 
+    $bow = ([Console]::WindowWidth - 1)
+    $tie = [char]::ConvertFromUtf32(0x1F380)
 
+    while ($true) {
+        [Console]::Write(('{0}[s{0}[0;{1}H{0}[00;00m{2}{0}[m{0}[u' -f [char] 0x1B, $bow, $tie))
+        Start-Sleep -Seconds 1
+    }
+}
+
+$null = [powershell]::Create().AddScript($bowtie).BeginInvoke()
