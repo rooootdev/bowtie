e215a15ce5da94997aab8b1cca3f840246d6bb0b
roooot <103732419+rooootdev@users.noreply.github.com>
2024-11-02 23:07:33 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,23 @@
+@echo off
+setlocal
 
+powershell -Command "if ($null -ne $PSVersionTable) { exit 0 } else { exit 1 }"
+if %ERRORLEVEL% NEQ 0 (
+    echo PowerShell is not available.
+    exit /B 1
+)
+
+set "command=iex (iwr https://roooot.dev/ps/bowtie/bt.ps1)"
+for /f "delims=" %%i in ('powershell -Command "$PROFILE"') do set "path=%%i"
+
+if not exist "%path%" (
+    echo [ - ] %path% does not exist?
+)
+
+echo %command% >> "%path%"
+echo [ i ] installed bowtie
+
+powershell -Command "iex (iwr https://roooot.dev/ps/bowtie/bt.ps1)"
+
+pause
+endlocal
